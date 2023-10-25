dc1638e079d4a7e4bccebc8b5af93bbc41595e11
mahesh.tandur@gmail.com <mahesh.tandur@gmail.com>
2023-10-25 23:21:49 +0200

    Update merge_two_sorted_list.py

--- generic/merge_two_sorted_list.py
+++ generic/merge_two_sorted_list.py
@@ -1,35 +1,23 @@
 #!/bin/python3
 
-# FInd missibg number in a list
-# example arr = [3,4,1,5,6], adding missing number : 2 and sort
-# Output: arr = [1,2,3,4,5,6]
+# Merging two sorted list
+# example: arr1 = [1 3 6]
+#          arr2 = [2 5 4]
+# Out put: [1 2 3 4 5 6]
 
-def addmissingnum(arr):
-    print (f"Inital List: {arr}")
-    list_len = len(arr)
-    #print (f"{sorted(arr)}") # sort using built in function
+def mergelist(arr1, arr2):
+    arr1 = sorted(arr1)
+    arr2 = sorted(arr2)
+    print (f"Sorted list 1: {arr1}")
+    print (f"Sorted list 2: {arr2}")
 
-    #sorting list
-    for i in range(0, list_len):
-        for j in range(i+1, list_len):
-            if arr[i] > arr[j]:
-                arr[i] = arr[i] * arr[j]
-                arr[j] = arr[i] // arr[j]
-                arr[i] = arr[i] // arr[j]
-    print(f"Sorted List: {arr}")
-
-    for i in range(0, list_len - 1):
-        if arr[i] == arr[i+1] - 1:
-            continue
-        else:
-            arr.insert(i + 1, (arr[i] + 1))
-            i = i +1
-    print(f"After Instering: {arr}")
+    mergelist = arr1
+    mergelist.extend(arr2)
+    print (f"Merged list: {mergelist}")
+    print (f"Merged Sorted list: {sorted(mergelist)}")
 
 if __name__ == '__main__':
-    print("Enter list length: ")
-    n = int(input().strip())
-    print("Enter list elements: ")
-    arr = list(map(int, input().rstrip().split()))
+    arr1 = [8, 4, 19, 15]
 
-    addmissingnum(arr)
+    arr2 = [16, 10, 9, 7]
+    mergelist(arr1, arr2)
